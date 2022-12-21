5143213fe07f7c521d6ba36c63db1b37ee919275
Saqib Suleman <saqibs3291@gmail.com>
2022-12-21 20:18:05 +0500

    added cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Card from "./components/Card";
+// import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -6,6 +8,18 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <div className="container">
+        <span>
+          <Card name={"Pakistan"} />
+          <Card name={"Australia"} />
+          <Card name={"USA"} />
+        </span>
+        <span>
+          <Card name={"Middle East"} />
+          <Card name={"Europe"} />
+          <Card name={"South Africa"} />
+        </span>
+      </div>
       <Footer />
     </div>
   );
